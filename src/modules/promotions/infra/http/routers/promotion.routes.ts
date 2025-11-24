4fb7fb98592f6ebd2ff218974820ae4032c8b510
art-83 <arthurfmedeiros77@gmail.com>
2025-11-24 10:12:10 -0300

    fix: endpoint schedule

--- src/modules/promotions/infra/http/routers/promotion.routes.ts
+++ src/modules/promotions/infra/http/routers/promotion.routes.ts
@@ -69,6 +69,6 @@ promotionRoutes.delete(
   promotionController.delete
 );
 
-promotionRoutes.delete("1", promotionController.deleteExpiredPromotions);
+promotionRoutes.delete("/scheduled/delete-all-expired-promotions", promotionController.deleteExpiredPromotions);
 
 export default promotionRoutes;
