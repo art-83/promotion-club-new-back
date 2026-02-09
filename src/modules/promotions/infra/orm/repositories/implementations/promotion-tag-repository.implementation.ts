7c5b860bff00e3ca19c6ce8a38ae7e41e0b0d2cb
arthur.medeiros2 <arthurfmedeiros77@gmail.com>
2026-02-08 23:43:47 -0300

    fix

--- src/modules/promotions/infra/orm/repositories/implementations/promotion-tag-repository.implementation.ts
+++ src/modules/promotions/infra/orm/repositories/implementations/promotion-tag-repository.implementation.ts
@@ -20,8 +20,8 @@ class PromotionTagRepository implements RepositoryProvider<PromotionTag> {
     if (options.tag_id) query.andWhere("promotion_tag.tag_id = :tag_id", { tag_id: options.tag_id });
     if (options.user_id) {
       query.leftJoinAndSelect("promotion_tag.promotion", "promotion");
-      query.leftJoinAndSelect("promotion.user", "user");
-      query.andWhere("user.id = :user_id", { user_id: options.user_id });
+      query.leftJoinAndSelect("promotion_tag.tag", "tag");
+      query.innerJoin("promotion.promotion_tickets", "promotion_tickets", "promotion_tickets.user_id = :user_id", { user_id: options.user_id });
     } 
       
     query.andWhere("promotion_tag.deleted_at IS NULL");
