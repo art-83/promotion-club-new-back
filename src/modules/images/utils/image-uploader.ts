49561773f302ddfd6d037f50e76d1361c110429b
arthur.medeiros2 <arthurfmedeiros77@gmail.com>
2026-02-14 18:31:48 -0300

    fix: image path

--- src/modules/images/utils/image-uploader.ts
+++ src/modules/images/utils/image-uploader.ts
@@ -3,7 +3,7 @@ import multer from "multer";
 import { v4 as uuidv4 } from "uuid";
 
 const imageStorage = multer.diskStorage({
-  destination: __dirname + "/../../storage/images",
+  destination: __dirname + "/../../../../storage/images",
   filename: (req, file, cb) => {
     const fileName = `${uuidv4()}-${file.originalname}`;
     return cb(null, fileName);
