fd88b0304cf1fe0e3f4ab8adf17fe5966d579f2b
art-83 <arthurfmedeiros77@gmail.com>
2026-02-17 11:12:18 -0300

    aaa

--- src/modules/users/services/user/show-users.service.ts
+++ src/modules/users/services/user/show-users.service.ts
@@ -12,9 +12,12 @@ class ShowUsersServices {
   public async execute(options: Partial<UserQueryOptionsDTO>) {
     // to /users/me
     if (options.id) {
-      const totalSpentByUser = await this.userRepository.totalSpentByUser(options.id);
+      const [user, totalSpentByUser] = await Promise.all([
+        this.userRepository.find({ id: options.id }),
+        this.userRepository.totalSpentByUser(options.id),
+      ]);
       return {
-        ...options,
+        ...user,
         total_spent: totalSpentByUser,
       };
     }
