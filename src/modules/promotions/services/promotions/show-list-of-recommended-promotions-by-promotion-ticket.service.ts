7c68d123e19047bb3144989e668c54cb840fb8ef
art-83 <arthurfmedeiros77@gmail.com>
2026-02-21 21:26:10 -0300

    fallback

--- src/modules/promotions/services/promotions/show-list-of-recommended-promotions-by-promotion-ticket.service.ts
+++ src/modules/promotions/services/promotions/show-list-of-recommended-promotions-by-promotion-ticket.service.ts
@@ -4,7 +4,6 @@ import PromotionTicket from "../../../tickets/infra/orm/entities/promotion-ticke
 import PromotionRepositoryProvider from "../../infra/orm/repositories/providers/promotions-repository.providers";
 import PromotionTicketRepositoryProvider from "../../../tickets/infra/orm/repositories/providers/promotion-ticket-repository.provider";
 import PromotionQueryOptionsDTO from "../../dtos/promotions/promotion-query-options.dto";
-import PromotionTicketQueryOptionsDTO from "../../../tickets/dtos/promotion-ticket-query-options.dto";
 
 @injectable()
 class ShowListOfRecommendedPromotionsByUser {
@@ -60,12 +59,11 @@ class ShowListOfRecommendedPromotionsByUser {
   }
 
   private async getUserTicketsWithTags(user_id: string): Promise<PromotionTicket[]> {
-    const promotionTicketQueryOptions = {
+    return this.promotionTicketRepository.find({
       user_id,
       join_promotion_tags: true,
       limit: this.TICKET_FETCH_LIMIT,
-    } as PromotionTicketQueryOptionsDTO;
-    return this.promotionTicketRepository.find(promotionTicketQueryOptions);
+    });
   }
 
   private extractTagFrequencyFromTickets(tickets: PromotionTicket[]): Map<string, number> {
